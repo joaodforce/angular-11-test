46e04a7b25cce8591ccb6b64fadecf7b9954c66e
João Eduardo <joao_eduardo_ss@icloud.com>
2020-11-12 17:07:02 -0300

    added basic support for reproducing issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FreBaseComponentsModule } from 'fre-base-components';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FreBaseComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
